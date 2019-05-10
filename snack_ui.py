e8e545817ce6a6dabaa57f1dd4b80e38c74ef1a6
Taylor Arndt <taylorarndt@wireless73-054.wireless.wmich.edu>
2019-05-10 12:54:55 -0400

    seeing how orka reacts when the label is the only thing in the UI

--- snack_ui.py
+++ snack_ui.py
@@ -1,6 +1,6 @@
 # to run make sure you are in the right directory and type test.py you should now have a window.
 # imports the application, the layout, the controls like buttons, and the widgets.      
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout,QPushButton, QLineEdit 
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout,QPushButton, QLineEdit, QMessageBox 
 # inicializes the application, you only need 1 per app.
 app = QApplication([])
 #  don't remove this line or else Orka will break!!
@@ -8,13 +8,8 @@ app = QApplication([])
 window = QWidget()
 # tells python we want to use the Vbox layout for our controls.
 layout = QVBoxLayout()
-# adds a button. 
-layout.addWidget(QPushButton('clear'))
-layout.addWidget(QPushButton('charge'))
+# adds a button
 layout.addWidget(QLabel('total'))
-QLabel.setAccessibleName=='total'
-
-
 # sets the window layout.
 window.setLayout(layout)
 # shows the window on screen. Without this you will have no window for your application. 
