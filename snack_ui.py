a02c047d50e291adfd81d13b47842a0f6df4dc46
Taylor Arndt <taylorarndt@Taylors-MBP.cclub>
2019-05-13 16:54:14 -0400

    added cancel button and got ready for master commit

--- snack_ui.py
+++ snack_ui.py
@@ -1,20 +1,20 @@
 # to run make sure you are in the right directory and type test.py you should now have a window.
 # imports the application, the layout, the controls like buttons, and the widgets.      
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout,QPushButton, QLineEdit, QMessageBox 
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout,QPushButton, QLineEdit,QListWidget,QListWidgetItem
 # inicializes the application, you only need 1 per app.
 app = QApplication([])
-#  don't remove this line or else Orka will break!!
 # makes a window for the application.
 window = QWidget()
 # tells python we want to use the Vbox layout for our controls.
 layout = QVBoxLayout()
-# adds a button layout.addChildLayout.addWidget(QPushButton('cancel'))
+# adds  buttons and other UI elements to the app.
+
 layout.addWidget(QPushButton('charge'))
 layout.addWidget(QLineEdit(' your total is '))
+layout.addWidget(QPushButton('cancel'))
 # sets the window 
 window.setLayout(layout)
 # shows the window on screen. Without this you will have no window for your application. 
 window.show()
-# closes the app. If this statement isn't here, you will have an application which doesn't exit. 
 app.exec_()
 
