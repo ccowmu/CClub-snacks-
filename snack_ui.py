dc5d0b030b61c7b900bb91f0a13157252d1771b5
taylor arndt <taylor.e.arndt@wmich.edu>
2019-05-28 19:07:33 -0400

    made the total box look more like a label.

--- snack_ui.py
+++ snack_ui.py
@@ -1,5 +1,6 @@
 #imports dependencies
-from PyQt5.QtWidgets import QApplication, QWidget,QGridLayout  ,QPushButton, QLineEdit,QListWidget,QListWidgetItem, QListView
+from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import *
 # initializes the application, you only need 1 per app.
 app = QApplication([])
 # makes a window for the application.
@@ -7,18 +8,31 @@ window = QWidget()
 
 #declares variables for our UI 
 total=QLineEdit('your total is')
+grid_layout = QGridLayout()
+
 charge=QPushButton('charge')
+total=QLineEdit()
+total.setText('total')
+total.setEnabled(False)
+total.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Minimum)
+window.setStyleSheet("""
+	.QLineEdit {
+		border-style: none;
+		color: black;
+		background:transparent;
+		qproperty-iconSize: 10px 10px;
+		}
+	""")
+
 cancel=QPushButton('cancel')
 
 # tells python to use the grid layout class.
-grid_layout = QGridLayout()
 grid_layout.setSpacing(10)
 grid_layout.addWidget(charge, 1,0)
 grid_layout.addWidget(cancel, 1,3)
-grid_layout.addWidget(total, 1,2)
+grid_layout.addWidget(total, 2,6)
 
 #QListWidget for displaying snack list at checkout
-
 #adds dictionary for generation of snack buttons.  
 snacks=QListWidget()
 popcorn={ 'name': 'popcorn', 'type': 'snack', 'cost': '0.25',}
@@ -33,7 +47,7 @@ p_button=""
 for key in pop:
  	p_button=p_button+str(pop[key])
 p_button=QPushButton(p_button)
-`grid_layout.addWidget(p_button)
+grid_layout.addWidget(p_button)
 
 # code which is needed to exit any app.
 window.setLayout(grid_layout)
